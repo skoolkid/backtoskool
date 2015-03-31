333de7c6e1da9c4a77e2507312b12377a98928b2
Richard Dymond <rjdymond@gmail.com>
2015-03-31 10:58:47 -0300

    Commit version 20140614

---
+++ microsphere.js
@@ -0,0 +1,7 @@
+function s(bubbleId) {
+  bubble = document.getElementById(bubbleId);
+  bubble.style.display = bubble.style.display == "block" ? "" : "block";
+}
+function h(bubble) {
+  bubble.style.display = "";
+}
